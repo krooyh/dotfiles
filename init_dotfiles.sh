56c94883d20eb9ced611f48a58518eaa90e79bbf
Rafał Kreush <rkreusch@gog.com>
2019-05-10 16:31:36 +0200

    init script fixes

--- init_dotfiles.sh
+++ init_dotfiles.sh
@@ -5,21 +5,23 @@ files=".aliases .dotfiles .exports .functions .plugins .tmux.conf .zshrc"
 cd ${dir}
 
 if [[ ! -d ${old_dir} ]]; then
-echo "Creating old directory"
-mkdir ${old_dir}
+    echo "Creating old directory"
+    mkdir ${old_dir}
 fi
 
+#todo: check if it's not directory
+#todo: handle case if it's link but not to file that I want
 for file in ${files}; do
     if [[ ! -L ~/${file} ]]; then #if file is not link
-        #move to old dir if is file
-        if [[ -f ~/${file} ]]; then
-        echo "Move $file to old dir"
-        mv ~/${file} ${old_dir}
+        if [[ -f ~/${file} ]]; then #move to old dir if it's file
+            echo "Move $file to old dir"
+            mv ~/${file} ${old_dir}
         fi
 
        echo "Creating symlink to $file in home directory."
-        ln -s ${dir}/${file} ~/${file}
+       ln -s ${dir}/${file} ~/${file}
     fi
 done
 
+echo "Reading ~/.dotfiles"
 source ~/.dotfiles
